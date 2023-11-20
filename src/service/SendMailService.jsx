649c9d5bd0d2763d14b2c8952757b8ed2ac971e3
leiner <leinermenar@outlook.com>
2023-11-20 14:01:59 -0500

    Ajustes en el sistema

--- src/service/SendMailService.jsx
+++ src/service/SendMailService.jsx
@@ -4,20 +4,15 @@ import axios from "axios"
 const sendMailBulk = async (datas) => {
     let datos = {}
     try {
-
+    const { VITE_API_URL } = import.meta.env
     const response = await axios
-      .post("http://localhost:3000/api/enviar-correo", datas, {
+      .post(`${VITE_API_URL}/enviar-correo`, datas, {
         headers: {
           "Content-Type": "application/json",
         },
       })
-      .then((response) => {
-        datos =response.data
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-
+      
+      datos = response.data
     } catch (error) {
         console.error("Error creating clients:", error);
         throw error;
